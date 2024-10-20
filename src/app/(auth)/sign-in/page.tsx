f6ad07f5439e43b6a012d76a98a8381545027a14
tobisamcode <samueloluwatobiloba48@gmail.com>
2024-10-20 01:41:19 +0100

    setting up my boilerplate

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { SignInCard } from "@/features/auth/components/sign-in-card";
+import React from "react";
+
+const Page = () => {
+  return <SignInCard />;
+};
+
+export default Page;
